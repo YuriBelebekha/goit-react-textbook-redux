814cc51df50f71ae59862a1e0d1bdaacabcb3041
Yuri Belebekha <ybelebekha@gmail.com>
2023-03-26 22:29:43 +0300

    Added components and redux

---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { devToolsEnhancer } from '@redux-devtools/extension';
+import { rootReducer } from './reducer';
+
+const enhancer = devToolsEnhancer();
+export const store = configureStore({ reducer: rootReducer }, enhancer);
